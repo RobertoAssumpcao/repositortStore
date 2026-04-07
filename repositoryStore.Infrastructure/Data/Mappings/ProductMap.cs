1d257f0a8ae17c7a6b54f7476728d0a146cc6e96
Roberto Assumpção <contatobetoassumpcao@icloud.com>
2026-04-06 21:12:49 -0300

    Add ProductMap configuration for Entity Framework

---
+++ repositoryStore.Infrastructure/Data/Mappings/ProductMap.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using repositoryStore.Domain.Entities;
+
+namespace repositoryStore.Infrastructure.Data.Mappings;
+
+public class ProductMap : IEntityTypeConfiguration<Product>
+{
+    public void Configure(EntityTypeBuilder<Product> builder)
+    {
+        builder.ToTable("Product");
+        
+        builder.HasKey(x => x.Id);
+        
+        builder.Property(x => x.Title)
+            .IsRequired(true)
+            .HasMaxLength(160)
+            .HasColumnType("varchar");
+    }
+}
