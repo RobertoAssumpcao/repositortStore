b7299f8d85711d4602c0017991c411b102b13f06
Roberto Assumpção <contatobetoassumpcao@icloud.com>
2026-04-11 20:10:41 -0300

    Add ISpecification interface and abstract Specification class for query specifications

---
+++ repositoryStore.Domain/Abstractions/ISpecification.cs
@@ -0,0 +1,9 @@
+﻿using System.Linq.Expressions;
+
+namespace repositoryStore.Domain.Abstractions;
+
+public interface ISpecification<T>
+{
+    Expression<Func<T, bool>> ToExpression();
+    bool IsSatisfiedBy(T entity);
+}
